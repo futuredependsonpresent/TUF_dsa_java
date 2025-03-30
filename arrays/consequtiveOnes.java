e18e7717fe8b52a8347e88904cd7f87272ec8f0f
rohitmr <futuredependsonpresent@gmail.com>
2025-03-30 21:08:25 +0530

    added the code for consequtiveOnes.java

---
+++ arrays/consequtiveOnes.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class consequtiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans=0;
+        int consecutiveOnes = 0;
+        for(int i:nums)
+        {
+            if(i==1)
+            {
+                ans+=1;
+                consecutiveOnes=Math.max(consecutiveOnes,ans);
+            }
+            else
+            {
+                ans=0;
+            }
+        }
+        return consecutiveOnes;
+    }
+}
